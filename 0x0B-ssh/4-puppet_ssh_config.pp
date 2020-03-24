3e167c340bc79abcee7aab16d1cd48c799e58d32
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-03-24 23:54:11 +0100

    advanced

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Client configuration file (w/ Puppet)
-exec {'Puppet':
-path     => '/bin'
+exec {'/etc/ssh/ssh_config':
+path     => '/bin',
 command  => 'echo "PasswordAuthentication no\n  IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
 provider => 'shell',
 }
